77ca35d034c534945dc49e1faf2f2df98f0092ec
fernvilla <fvilla18@gmail.com>
2015-11-11 23:29:59 -0800

    Wrap up the config

--- kanban_app/webpack.config.js
+++ kanban_app/webpack.config.js
@@ -1,32 +1,43 @@
 var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
+var merge = require('webpack-merge');
 
+var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH, 'app');
-var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
 
-module.exports = {
+var common = {
     entry: APP_PATH,
-    output: {
-        path: BUILD_PATH,
-        filename: 'bundle.js'
-    },
-    devServer: {
-        historyApiFallback: true,
-        hot: true,
-        inline: true,
-        progress: true,
-
-        //parse host and port from env so this
-        //is easy to customize
-        host: process.env.HOST,
-        port: process.env.PORT
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+                include: APP_PATH
+            }
+        ]
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin(),
+        // important! move HotModuleReplacementPlugin below
+        //new webpack.HotModuleReplacementPlugin(),
         new HtmlwebpackPlugin({
-            title: 'Kanban app'
+          title: 'Kanban app'
         })
     ]
 };
+
+if(TARGET === 'start' || !TARGET) {
+    module.exports = merge(common, {
+        devtool: 'eval-source-map',
+        devServer: {
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+    });
+}
