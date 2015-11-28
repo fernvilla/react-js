8b97a523b5cae698f13e7b7435ad37080789bd58
fernvilla <fvilla18@gmail.com>
2015-11-27 17:04:56 -0800

    Fix some setup stuff

--- kanban_app/webpack.config.js
+++ kanban_app/webpack.config.js
@@ -3,40 +3,55 @@ var HtmlwebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
 var merge = require('webpack-merge');
 
-var TARGET = process.env.npm_lifecycle_event;
-var ROOT_PATH = path.resolve(__dirname);
-var APP_PATH = path.resolve(ROOT_PATH, 'app');
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build')
+};
 
 var common = {
-    entry: APP_PATH,
+    entry: PATHS.app,
     resolve: {
-            extensions: ['', '.js', '.jsx']
+        extensions: ['', '.js', '.jsx']
     },
     module: {
         loaders: [
             {
                 test: /\.css$/,
                 loaders: ['style', 'css'],
-                include: APP_PATH
+                include: PATHS.app
+            },
+            {
+                test: /\.jsx?$/,
+                loaders: ['babel'],
+                include: PATHS.app
             }
         ]
     },
     plugins: [
         new HtmlwebpackPlugin({
-          title: 'Kanban app'
+            title: 'Kanban app'
         })
     ]
 };
 
 if(TARGET === 'start' || !TARGET) {
     module.exports = merge(common, {
-        devtool: 'eval-source-map',
-        devServer: {
-            historyApiFallback: true,
-            hot: true,
-            inline: true,
-            progress: true
-        },
+    devtool: 'eval-source-map',
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true,
+
+        // display only errors to reduce the amount of output
+        stats: 'errors-only',
+
+        // parse host and port from env so this is easy
+        // to customize
+        host: process.env.HOST,
+        port: process.env.PORT
+    },
         plugins: [
             new webpack.HotModuleReplacementPlugin()
         ]
